4722f6681d2963a71d4e0199c65fcb5239c08353
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-08-22 11:54:59 -0400

    Update gameData.js

--- gameData.js
+++ gameData.js
@@ -1,40 +1,51 @@
 //Static Data
 
 var mapData = {
-  mapXD: {
-    mapYD: {
-      area: {
-        id: [0],
-        localX: [],
-        localY: [],
-        width: [],
-        height: []
-      },
+  mapX0: {
+    mapY0: {
+      area: false,
       wall: {
         localX: [],
         localY: [],
         width: [],
         height: []
       },
-      door: {
-        type: ["n", "t", "s", "s"],
-        id: [0, 0, 0, 1],
-        localX: [],
-        localY: [],
-        width: [],
-        height: []
-      },
-    }
-  },
-  mapX0: {
-    mapY0: {
-      area: false,
-      wall: false,
       door: false,
       item: false,
-      switch: false,
+      locks: false,
+      cannon: false,
+      teleport: {
+        localX: [],
+        localY: [],
+        type: [1]
+      }
     }
   }
 };
 
 //Dynamic Data
+
+var playerData = {
+  location: {
+    localX: 45,
+    localY: 45,
+    globalX: 0,
+    globalY: 0,
+    velocityX: 0,
+    velocityY: 0
+  },
+  stats: {
+    power: 1000,
+    hp: 3,
+  },
+  locks: {
+    normal: [],
+    timed: [],
+    swap: false
+  },
+  items: {
+    power: [],
+    dash: [],
+    hp: []
+  }
+};
