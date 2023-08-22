16756c2f730b864f7bce0b36c6ee01fea644a8dc
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-08-22 13:09:18 -0400

    Update renderEngine.js

--- renderEngine.js
+++ renderEngine.js
@@ -3,7 +3,7 @@ function backgroundUpdate() {
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "#777777";
   ctx.fillRect(0, 0, 1366, 768);
-  var mapDraw = mapLoad(0);
+  var mapDraw = mapLoad(1);
   if (mapDraw != false) {
     ctx.fillStyle = "#000000";
     for (var x = 0; x < mapDraw.localX.length; x++) {
