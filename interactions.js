275308c41d12ce4042bf814dad42dfb61a8ed64f
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-08-22 14:34:14 -0400

    Update interactions.js

--- interactions.js
+++ interactions.js
@@ -1,12 +1,29 @@
 function unlock(type, id, timer) {
   switch (type) {
     case "n":
+      for (var x = playerData.locks.normal.length; x < id; x++) {
+        playerData.locks.normal[x] = false;
+      }
+      playerData.locks.normal[id] = true;
       break;
     case "t":
+      for (var x = playerData.locks.timed.length; x < id; x++) {
+        playerData.locks.timed[x] = false;
+      }
+      playerData.locks.timed[x] = true;
+      if (timer > 0) {
+        playerData.meta.warpLock = true;
+        playerData.meta.timeLimit = setTimeout(warp(), timer);
+      } else {
+        clearTimeout(timeLimit);
+        playerData.meta.warpLock = false;
+      }
       break;
     case "s":
       if (id == 1) {
-        playerData = true;
+        playerData.locks.swap = true;
+      } else {
+        playerData.locks.swap = false;
       }
       break;
   }
