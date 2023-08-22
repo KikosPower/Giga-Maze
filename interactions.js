dc3778ab175841865cb7d63019d343b8d25f3253
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-08-22 14:17:25 -0400

    Create interactions.js

---
+++ interactions.js
@@ -0,0 +1,13 @@
+function unlock(type, id, timer) {
+  switch (type) {
+    case "n":
+      break;
+    case "t":
+      break;
+    case "s":
+      if (id == 1) {
+        playerData = true;
+      }
+      break;
+  }
+}
