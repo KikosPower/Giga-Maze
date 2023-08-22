0a68d00e4187213fc3209b179a2c3f13755b9999
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-08-22 11:39:45 -0400

    Create gameData.js

---
+++ gameData.js
@@ -0,0 +1,32 @@
+//Static Data
+
+var mapData = {
+  mapXD: {
+    mapYD: {
+      area: {
+        id: [],
+        localX: [],
+        localY: [],
+        width: [],
+        height: []
+      },
+      wall: {
+        localX: [],
+        localY: [],
+        width: [],
+        height: []
+      },
+    }
+  },
+  mapX0: {
+    mapY0: {
+      area: false,
+      wall: false,
+      door: false,
+      item: false,
+      switch: false,
+    }
+  }
+};
+
+//Dynamic Data
