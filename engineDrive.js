f47c1e88e9ed573d0b14f2989c2c93c3db9b7148
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-10-30 12:17:56 -0400

    Update engineDrive.js

--- engineDrive.js
+++ engineDrive.js
@@ -8,6 +8,43 @@ document.onkeydown = function(e) {
     case playerData.controls.up:
       metaData.input.up = true;
       break;
+    case playerData.controls.down:
+      metaData.input.down = true;
+      break;
+    case playerData.controls.left:
+      metaData.input.left = true;
+      break;
+    case playerData.controls.right:
+      metaData.input.right = true;
+      break;
+    case playerData.controls.dash:
+      metaData.input.dash = true;
+      break;
+    case playerData.controls.interact:
+      metaData.input.interact = true;
+      break;
   }
-  console.log(e.code);
+}
+
+document.onkeyup = function(e) {
+  switch (e.code) {
+    case playerData.controls.up:
+      metaData.input.up = false;
+      break;
+    case playerData.controls.down:
+      metaData.input.down = false;
+      break;
+    case playerData.controls.left:
+      metaData.input.left = true;
+      break;
+    case playerData.controls.right:
+      metaData.input.right = false;
+      break;
+    case playerData.controls.dash:
+      metaData.input.dash = false;
+      break;
+    case playerData.controls.interact:
+      metaData.input.interact = false;
+      break;
+  }
 }
