a21c8f689a6aba396573149327ea9b9c1eaa63d5
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-10-30 11:21:02 -0400

    Update renderEngine.js

--- renderEngine.js
+++ renderEngine.js
@@ -10,4 +10,19 @@ function backgroundUpdate() {
       ctx.fillRect(mapDraw.localX[x], mapDraw.localY[x], mapDraw.width[x], mapDraw.height[x]);
     }
   }
+  mapDraw = mapLoad(2);
+  if (mapDraw != false) {
+    for (var x = 0; x < mapDraw.localX.length; x++) {
+      switch (mapDraw.type[x]) {
+        case 0:
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    }
+  }
 }
