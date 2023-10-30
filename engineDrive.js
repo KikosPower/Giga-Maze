060cb5d4d974afa096210754008935299acfbc7f
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-10-30 12:13:10 -0400

    Update engineDrive.js

--- engineDrive.js
+++ engineDrive.js
@@ -1,4 +1,12 @@
-function mainFrame() {
+/*function mainFrame() {
   if (metaData.programRun) {
+  }
+}*/
+
+document.onkeydown = function(e) {
+  switch (e.code) {
+    case playerData.input.up:
+      metaData.input.up = true;
+      break;
   }
 }
