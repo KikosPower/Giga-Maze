fa25f07796c99d36fbfd5bab6f51d126edebf77a
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-10-30 12:54:19 -0400

    Create saveSystem.js

---
+++ saveSystem.js
@@ -0,0 +1,15 @@
+var lastSave = 0;
+
+function fileCheck() {
+  if (localStorage.file) {
+    console.log("Continue");
+  } else {
+    console.log("New Game");
+  }
+}
+
+function save() {}
+
+function load() {}
+
+function newGame() {}
