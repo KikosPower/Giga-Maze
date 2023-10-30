e26c54d52fba1d0239ab7565127da611c3f29618
KikosPower <142911193+KikosPower@users.noreply.github.com>
2023-10-30 13:49:28 -0400

    Update saveSystem.js

--- saveSystem.js
+++ saveSystem.js
@@ -1,15 +1,27 @@
 var lastSave = 0;
 
 function fileCheck() {
+  localStorage.setItem("default", 0);
   if (localStorage.getItem("file") == null) {
     document.getElementById("startScreen").style.display = "block";
   } else {
-    console.log("Continue");
+    lastSave = localStorage.getItem("file")
+    load(lastSave);
   }
 }
 
-function save() {}
+function save(file) {
+  if (metaData.warpLock == false) {
+    var saveData = JSON.stringify(playerData);
+    localStorage.setItem("save" + file, saveData);
+  }
+}
 
-function load() {}
+function load(file) {
+  var saveData = localStorage.getItem("save" + file);
+}
 
-function newGame() {}
+function newGame() {
+  load("default");
+  document.getElementById("startScreen").style.display = "none";
+}
