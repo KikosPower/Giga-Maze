6c2febcfa529cc442788641b394fa99348b1c056
KikosPower <142911193+KikosPower@users.noreply.github.com>
2024-11-05 17:18:26 -0500

    Update rendering.js

--- rendering.js
+++ rendering.js
@@ -1,6 +1,13 @@
 function backgroundDraw() {
+  //Loads the canvas, and paints the default background color.
   var canvas = document.getElementById("background");
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "#777777";
   ctx.fillRect(0, 0, 1440, 810);
+  //Starts drawing the walls
+  
 }
+
+var fakeMap = {
+  wall: [ ]
+};
