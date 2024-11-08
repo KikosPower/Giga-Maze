e132dee5d6908be4097ae79fad441a9df1a658f1
KikosPower <142911193+KikosPower@users.noreply.github.com>
2024-11-08 10:50:09 -0500

    Update rendering.js

--- rendering.js
+++ rendering.js
@@ -5,9 +5,18 @@ function backgroundDraw() {
   ctx.fillStyle = "#777777";
   ctx.fillRect(0, 0, 1440, 810);
   //Starts drawing the walls
-  
+  ctx.fillStyle = "#000000";
+  coord = fakeMap.wall;
+  ctx.beginPath();
+  for(let c in coord) {
+    if (coord[c] == "end") {
+      ctx.closePath();
+    }
+  }
 }
 
+//Development data file
+//don't make it into a coconut
 var fakeMap = {
   wall: [ ]
 };
